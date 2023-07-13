421032c83fc8406f202eb276f7825792f41a0b7d
Saivarunt <saivarunthirunavukarasu@gmail.com>
2023-07-13 10:51:10 +0530

    Post with fetch

--- jsapi-jsasync/app.js
+++ jsapi-jsasync/app.js
@@ -305,3 +305,17 @@ async function getData(){
 getData()
 
 
+//post req
+fetch('https://reqres.in/api/users',{
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+        name:'User 1'
+    })
+}).then(res=>{
+    return res.json()
+})
+    .then(data=>console.log(data))
+    .catch(error=>console.log(error))
